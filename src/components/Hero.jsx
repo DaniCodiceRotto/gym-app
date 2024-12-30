a7295e82f6b881442b88e5631d8063f7fddf8e2f
DaniCodiceRotto <daniel.mantovani@outlook.it>
2024-12-30 14:36:19 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ export default function Hero() {
         <p>IT'S Time To Get </p>
         <h1 className='uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Fuck'n <span className='text-blue-400'>Ripped</span></h1>
         </div>
-        <p className='text-sm md:text-base font-light'>Risk it all to get ripped—<span className='text-red-400 font-medium'>where you'll push limits you didn't even know you had,</span> sweat like you're auditioning for a waterfall commercial,
-            <span className='text-red-400 font-medium'> and walk away feeling like the champion of your own ridiculous fitness saga!</span></p>
+        <p className='text-sm md:text-base font-light'>Risk it all to get ripped—<span className='text-orange-300 font-medium'>where you'll push limits you didn't even know you had,</span> sweat like you're auditioning for a waterfall commercial,
+            <span className='text-orange-300 font-medium'> and walk away feeling like the champion of your own ridiculous fitness saga!</span></p>
         <Button func={()=>{
             window.location.href='#generate'
         }} text={"Accept And Begin" }></Button>
